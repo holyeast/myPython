621f4fa3f695b748b514dc549d2402541adc2576
holyeast <1079296426@qq.com>
2020-05-24 09:07:37 +0800

    init

--- myunittest/TestRunner.py
+++
@@ -1,8 +0,0 @@
-from test_twice import BaiduTest
-import unittest
-
-suite = unittest.TestSuite()
-suite.addTest(BaiduTest('test_baidu_news'))
-suite.addTest(BaiduTest('test_baidu_map'))
-unittest.TextTestRunner().run(suite)
-
